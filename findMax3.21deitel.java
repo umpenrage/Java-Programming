a6636263fd92ce0471f2707d52a4a3d471cefe9b
umpenrage <mark.umpenhour@gmail.com>
2017-06-02 17:34:46 -0400

    Update findMax3.21deitel.java

--- findMax3.21deitel.java
+++ findMax3.21deitel.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class Main{
 	
-	public static void main(Strings[] args){
+	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		int input1=0;
-		int input2=0;
-		int input3=0;
-		int input4=0;
-		int input5=0;
-		int input6=0;
-		int input7=0;
-		int input8=0;
-		int input9=0;
-		int input10=0;
+		long input1=0;
+		long input2=0;
+		long input3=0;
+		long input4=0;
+		long input5=0;
+		long input6=0;
+		long input7=0;
+		long input8=0;
+		long input9=0;
+		long input10=0;
 		int counter=1;
-		int MAX;
+		int comparisonCounter=2;
+		long MAX;
 		while(counter <=10){
 			System.out.println("Please input an integer for number " + counter);
-			int answer = input.nextInt();
+			long answer = input.nextLong();
 			switch(counter){
 				case 1: 
 							input1=answer;
@@ -56,34 +57,47 @@ public class Main{
 			counter++;
 		}// end while loop
 		
-		MAX = input1;
-		if(input2>MAX){
-			MAX=input2;
-		}
-		if(input3>MAX){
-			MAX=input3;
-		}
-		if(input4>MAX){
-			MAX=input4;
-		}
-		if(input5>MAX){
-			MAX=input5;
-		}
-		if(input6>MAX){
-			MAX=input6;
-		}
-		if(input7>MAX){
-			MAX=input7;
-		}
-		if(input8>MAX){
-			MAX=input8;
-		}
-		if(input9>MAX){
-			MAX=input9;
-		}
-		if(input10>MAX){
-			MAX=input10;
-		}
+		MAX = 0;
+		while(comparisonCounter<=10){
+			long comparison=0;
+			switch(comparisonCounter){
+			  case 1:
+			        comparison=input1;
+			        break;
+				case 2: 
+							comparison=input2;
+							break;
+				case 3: 
+							comparison=input3;
+							break;
+				case 4: 
+							comparison=input4;
+							break;
+				case 5: 
+							comparison=input5;
+							break;
+				case 6: 
+							comparison=input6;
+							break;
+				case 7: 
+							comparison=input7;
+							break;
+				case 8: 
+							comparison=input8;
+							break;
+				case 9: 
+							comparison=input9;
+							break;
+				case 10: 
+							comparison=input10;
+							break;
+				
+			}
+			if(comparison>MAX){
+				MAX=comparison;
+			}
+			comparisonCounter++;
+		} //end of whileloop
 		
 		System.out.println("The max number is " + MAX);
 		
