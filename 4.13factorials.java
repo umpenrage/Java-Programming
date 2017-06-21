583a3b6d19b906a8d079d1ea6f7e1e5d1a5b8577
umpenrage <mark.umpenhour@gmail.com>
2017-06-21 14:18:52 -0400

    Update 4.13factorials.java

--- 4.13factorials.java
+++ 4.13factorials.java
@@ -3,7 +3,7 @@ public class Main{
 	public static void main(String[] args){
 		int factorial=1;
 		for(int i=1; i<=20; i++){
-			factorial*=1;
+			factorial*=i;
 			System.out.printf("%,d", factorial);
 			if(i%5){
 				System.out.println();
