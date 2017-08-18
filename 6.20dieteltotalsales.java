21d6d9cd0a4307c6df21d9c9e91f0ea422eab535
umpenrage <mark.umpenhour@gmail.com>
2017-08-18 15:58:50 -0400

    Update 6.20dieteltotalsales.java

--- 6.20dieteltotalsales.java
+++ 6.20dieteltotalsales.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main{
 	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args){
-		
+		int grandTotal=0;
 		String[] names = new String[5];
 		int[][] product = {{254, 325, 1007, 5007, 6005}, {458, 562, 358, 8009, 9008}, {562, 780, 8002, 9008, 8765},{4587,968,654,589, 849}, {254, 325, 1007, 5007, 6005}};
 		for(int i = 0; i < names.length; i++){
@@ -13,16 +13,19 @@ public class Main{
 			
 		}
 		
-		System.out.println("\nEmployee\t\t\t 1\t 2\t 3\t 4\t 5\t\t total");
+		System.out.printf("\n%S %25d %15d %15d %15d %15d %26S\n","Employee", 1,2,3,4,5, "total");
+		
 		for(int i = 0; i < names.length; i++){
 		  int total = 0;
 		  System.out.printf("%-10S\t\t\t", names[i]);
       for(int k = 0; k < product[i].length; k++){
         
-				System.out.printf("%-,1d\t",product[i][k]);
+				System.out.printf("%-,8d\t",product[i][k]);
         total += product[i][k];
+        ;
         if(k==product[i].length-1){
           System.out.printf("\t%-,1d", total);
+          grandTotal +=total;
         }
           
       }
@@ -30,16 +33,17 @@ public class Main{
 			System.out.println("");
 		
 		}
-		System.out.print("================================================================================||======\n");
+		System.out.print("=====================================================================================================================||===========\n");
     System.out.printf("%-10S\t\t", "Total");
     int k = 0;
     
     for(int i = 0; i<product.length; i++){
       
       int total2 = getColumnSum(product, k);
-      System.out.printf("\t%-,5d", total2);
+      System.out.printf("\t%-,8d", total2);
       k++;
     }
+    System.out.printf("%15s %-,6d"," ", grandTotal);
   }
 
 	
